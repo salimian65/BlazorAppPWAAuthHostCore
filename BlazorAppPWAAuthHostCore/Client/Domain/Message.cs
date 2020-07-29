04b55be4608e73070e1d5820cb87845927b60e4c
m.salimian <m.salimian@SS-VDI41-172>
2020-07-29 12:13:46 +0430

    Add project files.

---
+++ BlazorAppPWAAuthHostCore/Client/Domain/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppPWAAuthHostCore.Client.Domain
+{
+    public class Message
+    {
+        private Message()
+        {
+        }
+
+        public Message(string username, string body, bool mine)
+        {
+            Id = new Guid();
+            Username = username;
+            Body = body;
+            Mine = mine;
+        }
+
+        [Key]
+        public Guid Id { get; private set; }
+        public string Username { get; private set; }
+        public string Body { get; private set; }
+        public bool Mine { get; private set; }
+
+        public string CSS => Mine ? "sent" : "received";
+    }
+}
